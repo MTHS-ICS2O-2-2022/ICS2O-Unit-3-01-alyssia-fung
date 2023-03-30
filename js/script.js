90f228acc664d3f07c09e375ee6d898eb1ba0296
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-03-30 16:20:28 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+"use strict"
+
+function calculate() {
+  // input
+  const sideA = parseInt(document.getElementById("sideA").value)
+  const sideB = parseInt(document.getElementById("sideB").value)
+  const height = parseInt(document.getElementById("height").value)
+
+  // process
+  const area = [(A + B) / 2] * height
+
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+}
